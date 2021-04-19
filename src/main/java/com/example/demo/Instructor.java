ddb56046c0e2f2e5f7072edc479d8056a6537c8e
Theresa <theresa@Gbrads-Mac.home>
2021-04-18 21:00:51 -0400

    Completed through part 9

--- src/main/java/com/example/demo/Instructor.java
+++ src/main/java/com/example/demo/Instructor.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Instructor extends Person implements Teacher {
--- src/main/java/com/example/demo/Instructors.java
+++ src/main/java/com/example/demo/Instructors.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
-public class Instructors {
+import java.util.Iterator;
 
+public class Instructors extends People<Instructor> {
 
+    public Instructors(Instructor...instructors) {
+        super(instructors);
+    }
+
+    public Iterator<Instructor> iterator() {
+        return super.findAll().iterator();
+    }
 }
