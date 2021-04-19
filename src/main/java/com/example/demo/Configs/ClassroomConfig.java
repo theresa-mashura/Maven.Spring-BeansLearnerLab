d48444b94e71e87384ca9fb924fc43afc9858a66
Theresa <theresa@Gbrads-Mac.home>
2021-04-18 21:53:38 -0400

    Completed through 10.3

---
+++ src/main/java/com/example/demo/Configs/ClassroomConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.Configs;
+
+import com.example.demo.Groups.Classroom;
+import com.example.demo.Groups.Instructors;
+import com.example.demo.Groups.Students;
+import org.springframework.context.annotation.Bean;
+
+public class ClassroomConfig {
+
+    @Bean
+    public Classroom currentCohort(Instructors instructors, Students students){
+        return new Classroom(instructors, students);
+    }
+
+    @Bean
+    public Classroom previousCohort(Instructors instructors, Students previousStudents) {
+        return new Classroom(instructors, previousStudents);
+    }
+}
